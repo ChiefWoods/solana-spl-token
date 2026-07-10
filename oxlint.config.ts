e7da9b0a41b0d54bed1e23ed69590670e4bfebb7
Chii Yuen <chii.yuen@hotmail.com>
2026-07-11 03:34:26 +0800

    chore: fmt files

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,25 +1,25 @@
-import { defineConfig } from "oxlint";
+import { defineConfig } from 'oxlint';
 
 export default defineConfig({
-    plugins: ["eslint", "import", "typescript", "oxc"],
-    ignorePatterns: ["docs", "lib", "test-ledger", "package-lock.json"],
+    plugins: ['eslint', 'import', 'typescript', 'oxc'],
+    ignorePatterns: ['docs', 'lib', 'test-ledger', 'package-lock.json'],
     options: {
         maxWarnings: 0,
         respectEslintDisableDirectives: true,
     },
     rules: {
-        "@typescript-eslint/ban-ts-comment": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-unused-vars": "off",
-        "@typescript-eslint/no-empty-interface": "off",
-        "@typescript-eslint/consistent-type-imports": "error",
-        "import/extensions": ["error", "always", { ignorePackages: true }],
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/no-empty-interface': 'off',
+        '@typescript-eslint/consistent-type-imports': 'error',
+        'import/extensions': ['error', 'always', { ignorePackages: true }],
     },
     overrides: [
         {
-            files: ["examples/**/*", "test/**/*"],
+            files: ['examples/**/*', 'test/**/*'],
             rules: {
-                "import/extensions": "off",
+                'import/extensions': 'off',
             },
         },
     ],
